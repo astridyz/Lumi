081d6d26b34d9a240ede4789608103480c5f5a9a
Astrid <sakurasylmere@gmail.com>
2024-05-20 12:43:33 -0300

    Built and published by Moonwave

--- assets/js/1c768e1c.90a5352a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[264],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/astridyz/Lumi/blob/master","baseUrl":"/Lumi/","classOrder":[{"section":"Components","classes":["Cache","Serializer","Listener"]},{"classes":["Containers","Session"]}],"apiCategories":[]}')}}]);
