eb923645f5e8c2d9368765b325921cae718feb4e
Astrid <sakurasylmere@gmail.com>
2024-05-22 16:10:31 -0300

    Built and published by Moonwave

---
+++ assets/js/ac93344b.4bfd395e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[217],{83373:e=>{e.exports=JSON.parse('[{"value":"Types","id":"types","level":2},{"value":"Properties","id":"properties","level":2},{"value":"Functions","id":"functions","level":2},{"value":".data","id":"data","level":3},{"value":".payload","id":"payload","level":3}]')}}]);
